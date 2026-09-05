ef04f1b30b00464784205e36ad0bdfe89707b541
dm-dmytrenko <dmytro.dmytrenko.prog@gmail.com>
2026-09-05 06:04:56 +0200

    handle authorization of a user

---
+++ src/app/auth.tsx
@@ -0,0 +1,39 @@
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { db } from "@/lib/db";
+import Credentials from "next-auth/providers/credentials";
+import { verifyPassword } from "@/lib/utils";
+
+export const { auth, signIn, signOut } = NextAuth({
+    adapter: PrismaAdapter(db),
+    session: { strategy: "jwt" },
+    providers: [
+        Credentials({
+            name: "Credentials",
+            credentials: {
+                email: { label: "Email", type: "email" },
+                password: { label: "Password", type: "password" }
+            },
+            async authorize(credentials) {
+                if (!credentials?.email || !credentials?.password) return null;
+
+                const user = await db.user.findUnique({
+                    where: { email: credentials.email as string }
+                })
+
+                if (!user || !user.password) return null;
+
+                const isValid = await verifyPassword(
+                    credentials.password as string, user.password
+                )
+
+                if (!isValid) return null;
+
+                return user;
+            }
+        })
+    ],
+    pages: {
+        signIn: "/login"
+    }
+})
